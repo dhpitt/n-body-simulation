fcbe6965822d8a004c6370229348f9038e5b0a7a
dhpitt <david.h.pitt@gmail.com>
2021-01-09 12:00:11 -0500

    bug fixes

--- n_body_main.py
+++ n_body_main.py
@@ -6,8 +6,8 @@
 import numpy as np
 from math import pi
 import time
-from scipy import integrate
-import plotly.graph_objects as go
+import astropy.units as u
+import astropy.constants as c
 import matplotlib.pylab as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as anim
@@ -18,9 +18,9 @@ from n_body_planet import pointMass
 
 # set up the simulation
 
-planet_1 = pointMass(0,1e7,0,0,1.0)
-planet_2 = pointMass(0,-3e7,9e4,0,0.012)
-planet_3 = pointMass(0.0,-7e6,-4e4,1e5,2.1)
+planet_1 = pointMass(0,1e7,0,-2e6,4.0)
+planet_2 = pointMass(-3e7,-5e7,9e5,0,2)
+planet_3 = pointMass(2e7,2e7,-4e4,2e5,4)
 planet_4 = pointMass(-8e6,-3e6,1e4,8e5,1.25)
 
 planets_2b = []
@@ -72,7 +72,7 @@ def RK4_solve_nBody(t_i,t_f,N,planets):
     #print(Ts)
     #print(len(Ts))
     numSteps = (t_f - t_i) / N 
-    dt = numSteps #*86400
+    dt = numSteps# *80 #86400
     #print("dt = " + str(dt))
     for p in planets:
             hPlace = p.history_[0]
@@ -87,8 +87,6 @@ def RK4_solve_nBody(t_i,t_f,N,planets):
             RK4_step(t,dt,p,planets)
             p.history_[t] = p.pos_
             p.velHist_[t] = p.vel_
-
-    #return planet_1.history_,planet_2.history_,planet_3.history_,planet_4.history_,Ts
     return [p.history_ for p in planets],Ts
 
 def RK4_step(t,dt,body,scene):
@@ -96,29 +94,36 @@ def RK4_step(t,dt,body,scene):
     # that outputs the next state of a planet (position and velocity)
     # given current state, history, and the scene.
 
-    y_old = np.array([body.history_[t-1],body.velHist_[t-1]])
+    y_old = np.array([body.pos_,body.vel_])
 
-    k1 = np.multiply(body.RK4_helper(dt, [0,0], scene), dt) 
-    k2 = np.multiply(body.RK4_helper(dt, np.multiply(0.5,k1), scene), dt)
-    k3 = np.multiply(body.RK4_helper(dt, np.multiply(0.5,k2), scene), dt)
-    k4 = np.multiply(body.RK4_helper(dt, k3, scene), dt)
+    k1 = np.multiply(body.RK4_helper(dt, np.array([0.,0.]), scene), dt) 
+    halfk1 = np.multiply(0.5,k1) # only take position for offset
+    #k2 = np.multiply(body.RK4_helper(dt, np.multiply(0.5,k1), scene), dt)
+    k2 = np.multiply(body.RK4_helper(dt, halfk1[0], scene), dt)
+    #k3 = np.multiply(body.RK4_helper(dt, np.multiply(0.5,k2), scene), dt)
+    halfk2 = np.multiply(0.5,k2)
+    k3 = np.multiply(body.RK4_helper(dt, halfk2[0], scene), dt)
+    k4 = np.multiply(body.RK4_helper(dt, k3[0], scene), dt)
     
     y_new = y_old + np.multiply((1/6.),(k1 + np.multiply(k2,2) + np.multiply(k3,2) + k4))
+    #y_new = y_old + dt*np.multiply((1/6.),(k1 + np.multiply(k2,2) + np.multiply(k3,2) + k4))
 
+    #print("y new = ")
+    #print(y_new)
     body.pos_ = y_new[0]
     body.vel_ = y_new[1]
 
 #a,b,c,tspace = RK4_solve_3body(0,20,100,planets_3b) # 3 body system output
 
 #results,tspace = RK4_solve_4body(0,20,50,planets_4b) # 4 body system output
-#results,tspace = RK4_solve_nBody(0,20,50,planets_2b) # 4 body system output
-results,tspace = RK4_solve_nBody(0,20,50,planets_3b) # 4 body system output
-
+results,tspace = RK4_solve_nBody(0,20,50,planets_2b) # 4 body system output
+#results,tspace = RK4_solve_nBody(0,20,50,planets_3b) # 3 body system output
 
 a = results[0]
 b = results[1]
-c = results[2]
 '''
+
+c = results[2]
 
 d = results[3]
 '''
@@ -135,15 +140,15 @@ ax.set(xlim=(-1e8,1e8),ylim=(-1e8,1e8))
 
 d1, = plt.plot(a[0][0],a[0][1],'ro',color='blue')
 d2, = plt.plot(b[0][0],b[0][1],'ro',color='green')
-d3, = plt.plot(c[0][0],c[0][1],'ro')
+#d3, = plt.plot(c[0][0],c[0][1],'ro')
 #d4, = plt.plot(d[0][0],d[0][1],'ro',color='black')
 
 
 def animate3b(i):
     d1.set_data(a[i][0],a[i][1])
     d2.set_data(b[i][0],b[i][1])
-    d3.set_data(c[i][0],c[i][1])
-    return d1,d2,d3,
+    #d3.set_data(c[i][0],c[i][1])
+    return d1,d2,#d3,
 
 '''
 def animate4b(i):
@@ -159,4 +164,4 @@ anim = anim.FuncAnimation(fig, animate3b, frames=tspace, \
                                       interval=50, blit=False, repeat=True)
 #plt.draw()
 plt.draw()
-anim.save('3body1.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
+anim.save('2bodytest.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
--- n_body_planet.py
+++ n_body_planet.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # All values in SI units
 real_G_const = 6.67e-11 # N*m^3/kg^2
-M_Earth = 5.972e26 #3e10 
+M_Earth = 5.972e26 #3e10  #3e10 
 
 # Note: add a measure to scale distance dynamically
 # or mass
@@ -18,51 +18,55 @@ class pointMass:
     # x,y,vx and vy.
 
     def __init__(self,x,y,v_naught_x,v_naught_y,m):
-        self.pos_ = [x,y]
-        self.vel_ = [v_naught_x,v_naught_y]
-        self.m_ = float(m * M_Earth)
+        self.pos_ = np.array([x,y])
+        self.vel_ = np.array([v_naught_x,v_naught_y])
+        self.m_ = m*M_Earth
         self.p_ = np.multiply(self.vel_, self.m_)
-        self.accel_ = [0.,0.]
+        self.accel_ = np.array([0.,0.])
         self.uTot_ = 0
         self.history_ = np.array([self.pos_])
         self.velHist_ = np.array([self.vel_])
 
     def gravitationOf(self,p2,offset):
-        # method to calculate gravitational force from p2 
-
-        r = [(p2.pos_[0] - (self.pos_[0]+offset[0])),(p2.pos_[1] - (self.pos_[1]+offset[1]))]
-        print("r = ")
-        print(r)
+        # method to calculate gravitational force from p2
+        
+        '''
+        print("self:")
+        print(self.pos_)
+        print("p2:")
+        print(p2.pos_)
+        print("offset:")
+        print(offset)
+        '''
+        #r = [(p2.pos_[0] - (self.pos_[0]+offset[0])),(p2.pos_[1] - (self.pos_[1]+offset[1]))]
+        r = p2.pos_-self.pos_+offset ##THIS IS RIGHT
+        #print("r = ")
+        #print(r)
         r_hat = np.divide(r,np.linalg.norm(r))
-        print("rhat = ")
-        print(r_hat)
+        #print("rhat = ")
+        #print(r_hat)
         fGrav = np.multiply(r_hat,(real_G_const*self.m_ * p2.m_) / (np.linalg.norm(r)**2))
         print("Grav =: ")
         print(fGrav)
         return [fGrav[0], fGrav[1]]
-    
+
     def RK4_helper(self,dt,offset,scene): 
         # helper function for Runge-Kutta 4 that gives an incremental
         # acceleration and velocity to the planet 
         
-        y = np.array([self.pos_,self.vel_])
+        #y = np.array([self.vel_,self.accel_])
 
         self.accel_ = [0.,0.]
         dt = float(dt)
         for p in scene:
             if self != p:
                 self.accel_ += np.true_divide(self.gravitationOf(p,offset),self.m_)
-        print("a")
-        print(y[0])
-        print("b")
-        print(np.multiply(self.vel_,dt))
-        print("c")
-        #print(0.5*np.multiply(self.accel_,dt**2))
+        print("a =:")
         print(self.accel_)
-        y[0] += np.multiply(self.vel_,dt) +  0.5*np.multiply(self.accel_,dt**2)
-        y[1] += np.multiply(self.accel_,dt)
-
-        return y
+        #y[0] += np.multiply(self.vel_,dt) +  0.5*np.multiply(self.accel_,dt**2)
+        velStep = 0.5*np.multiply(self.accel_,dt**2) # Update velocity
+        
+        return np.array([self.vel_+velStep,self.accel_])
     
     def calculateKE(self):
         # method to calculate kinetic energy
